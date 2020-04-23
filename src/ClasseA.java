5db385897eb8ff06077495e7f607d1c90f18d55c
Hernandes Andrade <cfhernandesandrade@gmail.com>
2020-04-23 17:56:51 -0300

    segundo commit

--- src/ClasseA.java
+++ src/ClasseA.java
@@ -1,6 +1,8 @@
 
 public class ClasseA {
 	
+	private String nome;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello, world;");
 	}
--- src/ClasseB.java
+++ src/ClasseB.java
@@ -1,4 +1,6 @@
 
 public class ClasseB {
+	
+	private int idade;
 
 }
